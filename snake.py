0ec9a3ec7bcf58c5671d93807e8b9a43df9f49ef
naut <jrbarnachea@gmail.com>
2022-05-09 18:10:58 -0700

    Update snake.py

--- snake.py
+++ snake.py
@@ -27,7 +27,7 @@ class Snake:
 
     def reset(self):
         for seg in self.segments:
-            seg.goto(1000, 1000) #sends dead snakes to some graveyard over in 1000, 1000
+            seg.goto(1000, 1000) # sends dead snakes to some graveyard over in 1000, 1000
         self.segments.clear()
         self.create_snake()
         self.head = self.segments[0]
